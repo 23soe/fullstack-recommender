7b3b894a6be31c64cbc33a92d9f97bdeb4da1d41
23soe <102648428+23soe@users.noreply.github.com>
2023-02-16 00:52:27 +0900

    genres 엔드포인트

--- app/resolver.py
+++ app/resolver.py
@@ -2,8 +2,17 @@ import pandas as pd
 
 item_fname = 'data/movies_final.csv'
 
+#/all 엔드포인트
 def random_items():
     movies_df = pd.read_csv(item_fname)
     movies_df = movies_df.fillna('') # 공백 채움
     result_items = movies_df.sample(n=10).to_dict("records")
+    return result_items
+
+#/genres 엔드포인트
+def random_genres_items(genre):
+    movies_df = pd.read_csv(item_fname)
+    genre_df = movies_df[movies_df['genres'].apply(lambda x : genre in x.lower())]
+    genre_df = genre_df.fillna('') # 공백 채움
+    result_items = genre_df.sample(n=10).to_dict("records")
     return result_items
